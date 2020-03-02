428646138ae660f75a85e326c881c2fed49bcace
aq_minato.naka <minato.naka@asia-quest.jp>
2020-03-02 17:50:49 +0900

    comments,entriesのmigration,model作成

---
+++ database/migrations/2020_03_02_083635_create_entries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEntriesTable extends Migration
+{
+    protected $table = 'entries';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->bigInteger('user_id', false, true);
+            $table->string('title', 85)->unique();
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
